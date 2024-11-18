ab35e39c9588785bdc0c34fe8b096bf3a50b7cfc
casuarinao8 <106784268+casuarinao8@users.noreply.github.com>
2024-11-18 13:14:34 +0800

    Astra child theme with custom code

---
+++ astra-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+// Enqueue the parent theme's style
+function astra_child_enqueue_styles()
+{
+    wp_enqueue_style('astra', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'astra_child_enqueue_styles');
+
+function enqueue_singpass_script()
+{
+    wp_enqueue_script('singpass-button-script', get_stylesheet_directory_uri() . '/js/singpass-button.js', array('jquery'), null, true);
+
+    // Pass the AJAX URL to the script
+    wp_localize_script('singpass-button-script', 'singpass_ajax', array(
+        'ajax_url' => admin_url('admin-ajax.php')
+    ));
+}
+add_action('wp_enqueue_scripts', 'enqueue_singpass_script');
