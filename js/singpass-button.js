efd660ff8ca012126d3bb3db1a1983fe475e2bab
casuarinao8 <106784268+casuarinao8@users.noreply.github.com>
2024-11-19 15:18:00 +0800

    Update singpass-button.js

--- js/singpass-button.js
+++ js/singpass-button.js
@@ -67,8 +67,12 @@ function populateNinjaFormFields(data) {
   const fullNameField = $('input[name="full_name"]');
   const emailField = $('input[name="email"]');
   const mobileNoField = $('input[name="mobile_no"]');
+  const dateOfBirthField = $("input.date_of_birth");
   const nricFinField = $('input[name="nric_fin"]');
-  const addressField = $('input[name="address"]');
+  // const sexField = $("select.sex");
+  // const nationalityField = $("select.nationality");
+  // const raceField = $("select.race");
+  const addressField = $('input[name="address"');
   const postalCodeField = $('input[name="postal_code"]');
 
   if (fullNameField.length) {
@@ -83,9 +87,25 @@ function populateNinjaFormFields(data) {
     mobileNoField.val(data.mobile_no).trigger("input").trigger("change");
   }
 
+  if (dateOfBirthField.length) {
+    dateOfBirthField.val(data.date_of_birth).trigger("input").trigger("change");
+  }
+
   if (nricFinField.length) {
     nricFinField.val(data.uinfin).trigger("input").trigger("change");
   }
+
+  // if (sexField.length) {
+  //   sexField.val(data.sex).trigger("input").trigger("change");
+  // }
+
+  // if (nationalityField.length) {
+  //   nationalityField.val(data.nationality).trigger("input").trigger("change");
+  // }
+
+  // if (raceField.length) {
+  //   raceField.val(data.race).trigger("input").trigger("change");
+  // }
 
   if (addressField.length) {
     addressField.val(data.address).trigger("input").trigger("change");
@@ -102,17 +122,37 @@ function lockFieldsIfFilled() {
   const $ = jQuery;
 
   const fullNameField = $('input[name="full_name"]');
+  const dateOfBirthField = $("input.date_of_birth");
   const nricFinField = $('input[name="nric_fin"]');
-  const addressField = $('input[name="address"]');
+  // const sexField = $("select.sex");
+  // const nationalityField = $("select.nationality");
+  // const raceField = $("select.race");
+  const addressField = $('input[name="address"');
   const postalCodeField = $('input[name="postal_code"]');
 
   if (fullNameField.length && fullNameField.val()) {
     fullNameField.prop("disabled", true).css("background-color", "#e0e0e0");
   }
 
+  if (dateOfBirthField.length && dateOfBirthField.val()) {
+    dateOfBirthField.prop("disabled", true).css("background-color", "#e0e0e0");
+  }
+
   if (nricFinField.length && nricFinField.val()) {
     nricFinField.prop("disabled", true).css("background-color", "#e0e0e0");
   }
+
+  // if (sexField.length && sexField.val()) {
+  //   sexField.prop("disabled", true).css("background-color", "#e0e0e0");
+  // }
+
+  // if (nationalityField.length && nationalityField.val()) {
+  //   nationalityField.prop("disabled", true).css("background-color", "#e0e0e0");
+  // }
+  
+  // if (raceField.length && raceField.val()) {
+  //   raceField.prop("disabled", true).css("background-color", "#e0e0e0");
+  // }
 
   if (addressField.length && addressField.val()) {
     addressField.prop("disabled", true).css("background-color", "#e0e0e0");
